5c2c5ee165f61d49e16aee9f7cef74e6fc65bd21
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-09-19 14:25:51 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+//findMatching()
+function findMatching(drivers, name) {
+    const comparison = driver => driver.toUpperCase() === name.toUpperCase();
+    if (comparison) {
+        return drivers.filter(comparison);
+    }
+    else return [];
+}
+
+//fuzzyMatch()
+function fuzzyMatch(drivers, startingLetters) {
+    const comparison = driver => driver.startsWith(startingLetters);
+    if (comparison) {
+        return drivers.filter(comparison);
+    }
+}
+
+//matchName()
+function matchName(drivers, str) {
+    const comparison = string => string.name === str;
+    return drivers.filter(comparison);
+}
